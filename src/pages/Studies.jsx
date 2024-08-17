1c3fee378f2d8f9324844391dbd92a23a7a40586
Norman Leyva <nleyvagarcia@gmail.com>
2024-08-18 00:46:08 +0200

    studies section initialized

--- src/pages/Studies.jsx
+++ src/pages/Studies.jsx
@@ -10,12 +10,17 @@ export const Studies = () => {
     <>
       <MyStudies>
         <h2>Breve biografía de estudios</h2>
-        <p>
+        <h3>
           Graduado del bachillerato oficial número 73, Tecámac Estado de México.
+        </h3>
+        <h3>
           Graduado del instituto de aprendizaje de la lengua árabe en Medina,
-          Arabia Saudita. Graduado de la facultad de las ciencias del hadith en
-          la universidad islámica de Medina.
-        </p>
+          Arabia Saudita.
+        </h3>
+        <h3>
+          Graduado de la facultad de las ciencias del hadith en la universidad
+          islámica de Medina.
+        </h3>
       </MyStudies>
     </>
   );
