5ed564f8182a26d0944b7730502c285b5c1ef6df
Norman Leyva <nleyvagarcia@gmail.com>
2024-08-28 20:40:00 +0200

    configuracion de react hook form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,4 @@
+import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
 
 const FormContainer = styled.div`
@@ -44,23 +45,73 @@ const Button = styled.button`
   padding: 1em 2em;
 `;
 
+const Span = styled.span`
+  display: block;
+  color: tomato;
+  font-size: x-small;
+`;
+
 export const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  console.log(errors);
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
   return (
     <FormContainer>
       <h1>Contacto</h1>
-      <Form>
+      <Form onSubmit={onSubmit}>
         <BodyForm>
           <ItemForm>
             <label>Nombre:</label>
-            <input style={Input} type="text" id="name" name="user-name" />
+            <input
+              style={Input}
+              type="text"
+              {...register('nombre', {
+                required: {
+                  value: true,
+                  message: 'Nombre es requerido',
+                },
+              })}
+            />
+            {errors.nombre && <Span>{errors.nombre.message}</Span>}
           </ItemForm>
           <ItemForm>
             <label>Correo electrónico:</label>
-            <input style={Input} type="email" id="mail" name="user_mail" />
+            <input
+              style={Input}
+              type="email"
+              {...register('email', {
+                required: {
+                  value: true,
+                  message: 'El correo es requerido',
+                },
+                pattern: {
+                  value: /^[a-z0-9._%+-]+@[a-z0-9·-]+\.[a-z]{2,4}$/,
+                  message: 'Correo no válido',
+                },
+              })}
+            />
+            {errors.email && <Span>{errors.email.message}</Span>}
           </ItemForm>
           <ItemForm>
             <label>Mensaje:</label>
-            <textarea style={TextArea} id="msg" name="user_mail" />
+            <textarea
+              style={TextArea}
+              {...register('mensaje', {
+                required: true,
+              })}
+            />
+            {errors.mensaje?.type === 'required' && (
+              <Span>Espacio requerido</Span>
+            )}
           </ItemForm>
           <DivButton>
             <Button type="submit">Enviar</Button>
